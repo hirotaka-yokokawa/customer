13e4e8510cb74bc7ea4ec389113c9bb266b4f2fc
Hirotaka <yokokawa.491992@gmail.com>
2018-11-20 16:29:49 +0900

    B-2. 年齢の追加｡print内を指定することでex:(print(ken.age))関数を使っていてもその数値だけを取り出せる｡

--- customer.py
+++ customer.py
@@ -4,18 +4,33 @@
 # tom = Customer(first_name="Tom", family_name="Ford").full_name()
 # tom.full_name()  # "Tom Ford" という値を返す
 
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# ken.age  # 15 という値を返す
+#
+# tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+# tom.age # 57 という値を返す
+#
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu.age # 73 という値を返す
+
 class Customer:
-    def __init__(self,first_name, family_name):
+    def __init__(self,first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
     def display_profile(self):
-        print(f"{self.first_name} {self.family_nameR}")
+        print(f"{self.first_name} {self.family_name} {self.age}")
 
 
 if __name__ == "__main__":
-    ken = Customer(first_name="Ken", family_name="Tanaka")
-    ken.display_profile()
+    ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+    print(ken.age)
 
-    tom = Customer(first_name="Tom", family_name="Ford")
-    tom.display_profile()
+    tom = Customer(first_name="Tom", family_name="Ford", age=57)
+    print(tom.age)
+
+    ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+    print((ieyasu.age))
+
+
