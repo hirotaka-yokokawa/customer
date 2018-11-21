a7a7be29ea0a1f966ecc95a80d641d8f13fdd670
Hirotaka <yokokawa.491992@gmail.com>
2018-11-21 16:38:20 +0900

    応用｡|区切り

--- customer.py
+++ customer.py
@@ -5,7 +5,7 @@ class Customer:
         self.age = age
 
     def display_profile(self):
-        return f"{self.first_name} {self.family_name}    {self.age}"
+        return f"{self.first_name} {self.family_name}|{self.age}"#無理やり感k
 
     def entry_fee(self):
         if 75 <= self.age:
@@ -21,22 +21,22 @@ class Customer:
 
 
     def info_csv(self):
-        return f"{self.display_profile()}    {self.entry_fee()}"
+        print(self.display_profile(),self.entry_fee(),sep='|')
 
 
 if __name__ == "__main__":
     ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-    print(ken.info_csv())
+    ken.info_csv()
 
     tom = Customer(first_name="Tom", family_name="Ford", age=57)
-    print(tom.info_csv())
+    tom.info_csv()
 
     ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-    print(ieyasu.info_csv())
+    ieyasu.info_csv()
 
     yokokawa = Customer(first_name="Hirotaka", family_name="Yokokawa", age=2)
-    print(yokokawa.info_csv())
+    yokokawa.info_csv()
 
     sugano = Customer(first_name="Keita", family_name="Sugano", age=75)
-    print(sugano.info_csv())
+    sugano.info_csv()
 
